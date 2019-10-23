5f96c1abf70134483997bb6f190f1a3b65bbed95
andrej.lavrinovic <andrej.lavrinovic@smartbear.com>
2019-10-23 11:00:45 +0100

    jenkins webhook test

--- src/main/java/ie/altech/traineratorcore/TraineratorCoreApplication.java
+++ src/main/java/ie/altech/traineratorcore/TraineratorCoreApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TraineratorCoreApplication {
 
+	// add some code here
 	public static void main(String[] args) {
 		SpringApplication.run(TraineratorCoreApplication.class, args);
 	}
