ba84923c8157618a4aea2eb58a061c798b30b2b6
andrej.lavrinovic <andrej.lavrinovic@smartbear.com>
2019-10-28 19:34:56 +0000

    jenkins connection test 15

--- src/main/java/ie/altech/traineratorcore/TraineratorCoreApplication.java
+++ src/main/java/ie/altech/traineratorcore/TraineratorCoreApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TraineratorCoreApplication {
 
 	// add some code here
-	// test_14
+	// test_15
 	public static void main(String[] args) {
 		SpringApplication.run(TraineratorCoreApplication.class, args);
 	}
