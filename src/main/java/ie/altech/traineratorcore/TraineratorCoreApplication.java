c55d1d67a2ed0f2f2933a91a2bbff4b5b8abc047
andrej.lavrinovic <andrej.lavrinovic@smartbear.com>
2019-10-30 12:22:04 +0000

    github webhook test_37

--- src/main/java/ie/altech/traineratorcore/TraineratorCoreApplication.java
+++ src/main/java/ie/altech/traineratorcore/TraineratorCoreApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TraineratorCoreApplication {
 
 	// add some code here
-	// test_36
+	// test_37
 	public static void main(String[] args) {
 		SpringApplication.run(TraineratorCoreApplication.class, args);
 	}
